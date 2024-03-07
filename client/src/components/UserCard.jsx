d7310080fe37c903589356c2897eb20fcf4b76ce
vishal <88555537+vishu7im@users.noreply.github.com>
2024-03-07 21:38:09 +0530

    replace

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -10,23 +10,23 @@ const UserCard = ({ user }) => {
         <div className="relative pb-48 overflow-hidden">
           <img
             onClick={() => {
-              navigate(`/user/${user._id}`);
+              navigate(`/user/${user?._id}`);
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
               });
             }}
             className="absolute rounded-s-xl inset-0 h-full w-full object-contain"
-            src={user.profile}
-            alt={user.name}
+            src={user?.profile}
+            alt={user?.name}
           />
         </div>
         <div className="p-6">
-          <h2 className="text-xl font-bold text-gray-900 mb-2">{user.name}</h2>
-          <p className="text-gray-600 text-lg mb-4">{user.profession}</p>
+          <h2 className="text-xl font-bold text-gray-900 mb-2">{user?.name}</h2>
+          <p className="text-gray-600 text-lg mb-4">{user?.profession}</p>
           <div
             onClick={() => {
-              navigate(`/user/${user._id}`);
+              navigate(`/user/${user?._id}`);
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
@@ -36,12 +36,12 @@ const UserCard = ({ user }) => {
           >
             <p className="text-green-600 font-bold mr-2">Batch:</p>
             <p className="text-gray-700">
-              {user.startYear + "-" + user.endYear}
+              {user?.startYear + "-" + user?.endYear}
             </p>
           </div>
           <div
             onClick={() => {
-              navigate(`/user/${user._id}`);
+              navigate(`/user/${user?._id}`);
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
@@ -50,11 +50,11 @@ const UserCard = ({ user }) => {
             className="flex items-center mb-4"
           >
             <p className="text-green-600 font-bold mr-2">Roll No:</p>
-            <p className="text-gray-700">{user.rollNo}</p>
+            <p className="text-gray-700">{user?.rollNo}</p>
           </div>
           <div
             onClick={() => {
-              navigate(`/user/${user._id}`);
+              navigate(`/user/${user?._id}`);
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
@@ -63,13 +63,13 @@ const UserCard = ({ user }) => {
             className="flex items-center mb-4"
           >
             <p className="text-green-600 font-bold mr-2">Trade:</p>
-            <p className="text-gray-700">{user.Trade}</p>
+            <p className="text-gray-700">{user?.Trade}</p>
           </div>
           <div className="flex items-center justify-between mb-4">
             <div className="flex">
               {
                 <a
-                  href={user.linkdln}
+                  href={user?.linkdln}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-gray-600 hover:text-green-500 mr-4 transition-colors duration-200"
@@ -79,7 +79,7 @@ const UserCard = ({ user }) => {
               }
               {
                 <a
-                  href={user.facebook}
+                  href={user?.facebook}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-gray-600 hover:text-green-500 mr-4 transition-colors duration-200"
@@ -89,7 +89,7 @@ const UserCard = ({ user }) => {
               }
               {
                 <a
-                  href={user.twitter}
+                  href={user?.twitter}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-gray-600 hover:text-green-500 transition-colors duration-200"
@@ -100,7 +100,7 @@ const UserCard = ({ user }) => {
             </div>
             <button
               onClick={() => {
-                navigate(`/user/${user._id}`);
+                navigate(`/user/${user?._id}`);
                 window.scrollTo({
                   top: 0,
                   behavior: "smooth",
@@ -115,17 +115,17 @@ const UserCard = ({ user }) => {
           <p
             className="text-gray-700"
             onClick={() => {
-              navigate(`/user/${user._id}`);
+              navigate(`/user/${user?._id}`);
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
               });
             }}
           >
-            {user.about.slice(0, 80)}....{" "}
+            {user?.about.slice(0, 80)}....{" "}
             <span
               onClick={() => {
-                navigate(`/user/${user._id}`);
+                navigate(`/user/${user?._id}`);
                 window.scrollTo({
                   top: 0,
                   behavior: "smooth",
